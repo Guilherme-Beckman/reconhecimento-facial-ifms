3b7905f67aeae8ef333608b4cc98098a0f982db3
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2025-10-07 11:29:04 -0400

    fix: remove arquivos de testes que sao desnecessarios

--- count_faces.py
+++
@@ -1,5 +0,0 @@
-import face_recognition
-
-image = face_recognition.load_image_file("image-test.png")
-face_locations = face_recognition.face_locations(image)
-print(f"Encontrei {len(face_locations)} rosto(s).")
